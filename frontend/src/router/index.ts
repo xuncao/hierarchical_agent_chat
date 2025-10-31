ee481a1cc3ef29acaccd1b007efd71e28775a7af
xuncao <542189786@qq.com>
2025-10-31 23:41:06 +0800

    前端开发环境代理配置

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Chat from '../views/Chat/index.vue'
+import Chat from '../views/chat/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
